eeb80c188c451f93649c7f682bca2101b5445f26
CharcoalNuggets <dgin@mail.csuchico.edu>
2018-02-07 17:51:37 -0800

    Determines identity of characters

--- main.cpp
+++ main.cpp
@@ -5,20 +5,39 @@ using namespace std;
 
 int main()
 {
+  
+  char letter;
+  char letterCapital;
+  char letterLowercase;
 
   cout<<"What character do you want to know about?\n";
+  cin >> letter;
 
+  if (letter >= 'A' && letter <= 'Z')
+  {
+  letterCapital = letter;
+  
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  cout<< letterCapital <<" is an upper case letter!\n";
+  }
 
+  if (letter >= 'a' && letter <= 'z')
+  { 
+  letterLowercase = letter;
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  cout<< letterLowercase << " is a lower case letter!\n";
+  }
 
+  if ((letter <= 'A' && letter >= 'Z') || (letter <= 'a' && letter >= 'z'))
+  {
   //in all other cases...
   cout<<"?! Pssh. What are you talking about?\n";
-  
+  }
+
+    
   //no matter what they enter...
   cout<<"The ASCII value is: ";
+  cout<<(int)letter<<endl;
 
   return 0;
 }
